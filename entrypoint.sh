96885f031c15708fa11cf3c90a7bd4a630b555f6
Dennis Eijpe <dennis@de-programmeer.nl>
2017-08-21 13:50:11 +0000

    Binding to 127.0.0.1 doesn't work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-iptables -t nat -D PREROUTING -p tcp --dport 80  -j DNAT --to-destination 127.0.0.1 || true
-iptables -t nat -D PREROUTING -p tcp --dport 443  -j DNAT --to-destination 127.0.0.1 || true
-iptables -t nat -I PREROUTING -p tcp --dport 80  -j DNAT --to-destination 127.0.0.1
-iptables -t nat -I PREROUTING -p tcp --dport 443  -j DNAT --to-destination 127.0.0.1
+iptables -t nat -D PREROUTING -p tcp --dport 80  -j DNAT --to-destination 172.21.0.1 || true
+iptables -t nat -D PREROUTING -p tcp --dport 443  -j DNAT --to-destination 172.21.0.1 || true
+iptables -t nat -I PREROUTING -p tcp --dport 80  -j DNAT --to-destination 172.21.0.1
+iptables -t nat -I PREROUTING -p tcp --dport 443  -j DNAT --to-destination 172.21.0.1
 
 while sleep 3600; do :; done
